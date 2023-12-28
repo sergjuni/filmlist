5584d504c7c755fddaa1f169713cc56f8a33125a
Sergio Junior <sergio.junior@stormgroup.com.br>
2023-12-27 22:43:17 -0300

    removes unused libs and add the film route

--- api/src/server.js
+++ api/src/server.js
@@ -1,10 +1,8 @@
 import express from "express";
 import cors from "cors";
-import bcrypt from "bcrypt";
-import { openDb } from "./dbconfig.js";
 import cookieParser from "cookie-parser";
-import { createToken, validateToken } from "./JWT.js";
 import userRoutes from "./routers/userRoutes.js";
+import filmRoutes from "./routers/filmRoutes.js";
 
 const app = express();
 app.use(express.json());
@@ -12,5 +10,6 @@ app.use(cookieParser());
 app.use(cors());
 
 app.use("/user", userRoutes);
+app.use("/film", filmRoutes);
 
 app.listen(3000);
