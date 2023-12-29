e3a8ab88c386006e3efa98204a1c7fd32d1505a7
Sergio Junior <sergio.junior@stormgroup.com.br>
2023-12-28 21:12:28 -0300

    adds more options to the cors in the server but didnt resolve the middleware issue

--- api/src/server.js
+++ api/src/server.js
@@ -7,7 +7,11 @@ import filmRoutes from "./routers/filmRoutes.js";
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 app.use("/user", userRoutes);
 app.use("/film", filmRoutes);
